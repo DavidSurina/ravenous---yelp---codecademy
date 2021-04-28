f7a94aac19a8460fc750e6842eed0b5c0a80203d
David Surina <david.surina22@gmail.com>
2021-04-28 21:41:33 +0200

    moved testing object to app.js and created new businesses object for rendering purposes

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,10 +4,15 @@ import Business from '../Business/Business';
 
 import './BusinessList.css';
 
-export default function BusinessList() {
+export default function BusinessList({businesses}) {
+    
+    const renderedList = businesses.map((business)=> {
+        return <Business business={business} />
+    })
+
     return (
         <div className="BusinessList">
-
+            {renderedList}
         </div>
     )
 }
