e7443cccada7eb9e335aa8e94d0ec88b53cfdbfa
David Surina <david.surina22@gmail.com>
2021-05-01 13:45:49 +0200

    changed class properties to className

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -5,23 +5,23 @@ import "./Business.css";
 export default function Business({business}) {
   const {imageSrc, name, adress, city, zipCode, category, rating, reviewCount} = business; 
   return (
-    <div class="Business">
-      <div class="image-container">
+    <div className="Business">
+      <div className="image-container">
         <img
           src={imageSrc}
           alt=""
         />
       </div>
       <h2>{name}</h2>
-      <div class="Business-information">
-        <div class="Business-address">
+      <div className="Business-information">
+        <div className="Business-address">
           <p>{adress}</p>
           <p>{city}</p>
           <p>{zipCode}</p>
         </div>
-        <div class="Business-reviews">
+        <div className="Business-reviews">
           <h3>{category}</h3>
-          <h3 class="rating">{rating}</h3>
+          <h3 className="rating">{rating}</h3>
           <p>{reviewCount}</p>
         </div>
       </div>
